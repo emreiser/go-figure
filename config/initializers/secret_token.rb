263a48a040b7969a3ca8b1a9bc5dbac214167472
emreiser <emilie.reiser@gmail.com>
2014-02-02 16:14:32 -0500

    Setup new project with model for Categories. Migrated and seeded DB with initial categories.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Project1::Application.config.secret_key_base = 'edf356ee193343b015b4a940f5e4546544091054716bb17882eab9505bea76d2fbbbb0f040b0d88ec27924948b936216130616f3f606b86e6ef751e3cdd51031'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Project1::Application.config.session_store :cookie_store, key: '_project_1_session'
