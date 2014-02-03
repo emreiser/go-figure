228bc4bd596a51f8e06683866f130a5e4d416dc4
emreiser <emilie.reiser@gmail.com>
2014-02-03 11:07:34 -0500

    Added bootstrap gem and imported to stylesheet. Added httparty gem. Migrated db.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
+gem 'json'
+gem 'bootstrap-sass', '~> 3.1.0'
 
 # Use postgresql as the database for Active Record
 gem 'pg'
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140202211059) do
+ActiveRecord::Schema.define(version: 20140203133947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: true do |t|
     t.text     "name"
+    t.text     "url_end"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "countries", force: true do |t|
+    t.text     "name"
+    t.integer  "hdi_rank"
+    t.boolean  "select"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
