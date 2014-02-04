ed7f7b0f9458d2f70db4530502fb81666d4611ee
emreiser <emilie.reiser@gmail.com>
2014-02-03 23:36:14 -0500

    Tweaked answers and questions controllers to avoid repeated countries and check for save errors.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,19 +5,22 @@ class AnswersController < ApplicationController
 	end
 
 	def create
-		@answer = Answer.create!(answer_params)
+		@answer = Answer.create(answer_params)
 		@answer[:selected_country_id] = Country.find_by(name: params[:commit]).id
-		if @answer[:selected_country_id] == get_correct_answer(@answer)
+
+		if @answer[:selected_country_id] == get_correct_country(@answer)
 			@answer[:correct] = true
 		else
 			@answer[:correct] = false
 		end
-		@answer.save
 
-		redirect_to questions_path
+		if @answer.save
+			redirect_to questions_path
+		else
+		end
 	end
 
-	def get_correct_answer(answer)
+	def get_correct_country(answer)
 		score_country_1 = Score.find_by(criterion_id: answer.criterion_id, country_id: answer.country_1_id).score
 		score_country_2 = Score.find_by(criterion_id: answer.criterion_id, country_id: answer.country_2_id).score
 
@@ -32,7 +35,7 @@ class AnswersController < ApplicationController
 
 	private
 	def answer_params
-		params.require(:answer).permit(:country_1_id, :country_2_id, :criterion_id, :selected_country_id, :selected_country_id)
+		params.require(:answer).permit(:country_1_id, :country_2_id, :criterion_id, :selected_country_id, :positive)
 	end
 
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,21 +2,17 @@ class QuestionsController < ApplicationController
 
 	def index
 		@countries = Country.all.shuffle
-		@country_1 = @countries.sample
-		@country_2 = @countries.sample
+		@country_1 = @countries.first
+		@country_2 = @countries.last
 
 		@answer = Answer.new
 
 		@criterion = Criterion.all.shuffle.sample
-		@question = "Which country do you think had higher #{@criterion.name} ?"
+		@question = "Which country do you think had higher #{@criterion.name}?"
 	end
 
 	def show
 	end
-
-	def new_question
-	end
-
 
 end
 
