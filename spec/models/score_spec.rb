13102dabae69b235e1c541bb4433c808b03bb5c4
emreiser <emilie.reiser@gmail.com>
2014-02-10 12:54:27 -0500

    Wrote test for Score instance method find_country_score. 100% test coverage for Score, Category, Criterion, Answer models.

--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -7,4 +7,11 @@ describe Score do
 		it { should belong_to :criterion }
 	end
 
+	describe '.find_country_score' do
+		it 'returns a single Score when passed country ID and criterion ID' do
+			@score = Score.create(criterion_id: 9, country_id: 3, score: 99)
+			expect(Score.find_country_score(3, 9)).to eq @score.score
+		end
+	end
+
 end
